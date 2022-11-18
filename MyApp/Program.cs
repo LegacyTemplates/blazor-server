243ba87bc4a86015fbc8fd0a4ff609bcbe601c07
Demis Bellot <demis.bellot@gmail.com>
2022-11-18 13:30:01 +0800

    Update Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -43,7 +43,7 @@ app.UseServiceStack(new AppHost());
 
 BlazorConfig.Set(new()
 {
-    //UseInProcessClient = true,
+    UseInProcessClient = true,
     Services = app.Services,
     JSParseObject = JS.ParseObject,
     EnableLogging = app.Environment.IsDevelopment(),
